a77535fe23a6988d39d72f1c481288670c370869
Daan Beusink <daan.beusink@mhsglobal.com>
2023-09-01 14:17:06 +0200

    Added icons, refactored background.js

--- background.js
+++ background.js
@@ -14,12 +14,17 @@ function downloadFml() {
     });
 }
 
+function resetFmlDownloadState(){
+    browser.browserAction.disable();
+    browser.browserAction.setBadgeText({ text: ""});
+}
+
 let fml;
+let tabId;
 browser.runtime.onMessage.addListener(handleMessage);
 browser.browserAction.onClicked.addListener(downloadFml);
 browser.tabs.onUpdated.addListener(function(tabId, changeInfo) {
     if (changeInfo.status == "loading") {
-        browser.browserAction.disable();
-        browser.browserAction.setBadgeText({ text: ""});
+        resetFmlDownloadState();
     }
-})
+});
